66814b542a66f7e658b12a88ac783a1cf2b01797
PrajnaBhandary03 <prajna.unplugged@gmail.com>
2017-10-09 20:29:54 -0400

    IP

--- src/prajnaga_assignment1.c
+++ src/prajnaga_assignment1.c
@@ -22,7 +22,9 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h> 
 #include <sys/socket.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <strings.h>
 #include <string.h>
